e4f7f1594314d5ee3bf5c53dc33ceac4760e5f8b
Reegan <reegansuman@gmail.com>
2025-03-28 19:51:58 +0530

    opengl-day2 completed setting up opengl lib with GLEW..(static linking)

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
+#include<iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+
 
 int main(void)
 {
+  
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
+    
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
+   
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Error" << std::endl;
+    }
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -26,6 +35,7 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBegin(GL_TRIANGLES);
+        
         glVertex2f(-0.5f, -0.5f);
         glVertex2f(0.0f, 0.0f);
         glVertex2f(0.5f, -0.5f);
