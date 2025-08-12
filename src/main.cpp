05eeb51ccfe100389f15fa60bf6f9e3be9d3cd1f
Reegan <reegansuman@gmail.com>
2025-08-12 14:10:59 +0530

    index buffers

--- src/main.cpp
+++ src/main.cpp
@@ -91,7 +91,7 @@ int main(void)
     if (!glfwInit())
         return -1;
     
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Triangle Renderer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -101,19 +101,37 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
     if (glewInit() != GLEW_OK) {
         std::cout << "Error" << std::endl;
     }
 
-   float positions[6] = {
+   float positions[] = {
+       
         -0.5f, -0.5f,
-        0.0f, 0.0f,
-        0.5f, -0.5f
+         0.5f, -0.5f,
+         0.5f, 0.5f,
+         -0.5f, 0.5f
+
+         
+        
+        
     };
+
+   unsigned int indices[] = {
+       0,1,2,
+       2,3,0
+   };
    unsigned int buffer;
    glGenBuffers(1, &buffer);
    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
+   glBufferData(GL_ARRAY_BUFFER, 6 *2* sizeof(float),positions,GL_STATIC_DRAW);
+
+   unsigned int ibo;
+   glGenBuffers(1, &ibo);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 *  sizeof(unsigned int), indices, GL_STATIC_DRAW);
+   
    glEnableVertexAttribArray(0);
    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
    
@@ -128,13 +146,10 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,nullptr);
         
         
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.0f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+      
 
         ///* Swap front and back buffers */
         glfwSwapBuffers(window);
